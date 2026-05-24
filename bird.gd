bf36d419b12cc73d085d4917e39f73417645f347
Brook-28 <rajanas28n@gmail.com>
2026-05-24 23:57:13 +0300

    initial commit

---
+++ bird.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+var game_gravity = 5
+
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
