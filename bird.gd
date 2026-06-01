e63a563b38bdaba38aba5121bdb5ea56b40ba685
Brook-28 <rajanas28n@gmail.com>
2026-06-01 17:45:12 +0300

    made the bird jump and added collision

--- bird.gd
+++ bird.gd
@@ -1,12 +1,41 @@
 extends Area2D
+signal hit
 
-var game_gravity = 5
-
+@export var jump_force = -500.0
+@export var game_gravity = 5.0
+var velocity = Vector2.ZERO
+var screen_size
+var is_dead = false
 
 func _ready() -> void:
-	pass # Replace with function body.
+	screen_size = get_viewport_rect().size
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta: float) -> void:
 	
+	if is_dead:
+		
+		return
+	
+	velocity.y += game_gravity
+	position += velocity * delta
+	
+	if Input.is_action_just_pressed("Jump"):
+		velocity.y = jump_force
+	
+	if position.y >= screen_size.y:
+		is_dead = true
+		hide()
+		hit.emit()
+		$CollisionShape2D.set_deferred("disabled", true)
+
+		
+	position = position.clamp(Vector2.ZERO, screen_size)
+
+
+func _on_body_entered(_body: Node2D):
+	is_dead = true
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deferred("disabled", true)
